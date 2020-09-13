a3eb3663b729f90ddcfbf43385014f506aaa23df
Kevin6Liu <996534740@qq.com>
2020-09-13 21:26:26 +0800

    added class 'checkleapyear'

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,5 +1,5 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author Kevin6Liu
  */
 public class LeapYear {
 
@@ -13,7 +13,20 @@ public class LeapYear {
             System.out.printf("%d is not a leap year.\n", year);
         }
     }
-
+    /** Checks if year is a LeapYear.
+    */
+    public static boolean isLeapYear(int year){
+      if(year < 0){
+        System.out.println("Please enter a positive number!");
+        return false;
+      }
+      else if ((year%4 == 0 && year%100 != 0) || year%400 == 0){
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
@@ -30,4 +43,3 @@ public class LeapYear {
         }
     }
 }
-
