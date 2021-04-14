e6c38eea862a26c470c1f875bee55db1df66a12c
Kevin_Liu <40707961+Kevin6Liu@users.noreply.github.com>
2021-04-14 20:45:51 +0800

    Added ArrayDeque
    1.0 version

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,163 @@
+/** Array based List */
+/* Invariants :
+1. The last item is always a[nextLast - 1]
+2. The first item is always a[nextFirst + 1]
+ */
+public class ArrayDeque {
+    private int[] a;
+    private int size = 0;
+    private int nextFirst = 4;
+    private int nextLast = 5;
+    private int factor = 2;
+    /** Creates an empty list. */
+    public ArrayDeque() {
+        a = new int[8];
+    }
+
+    /** Increase the volume of the original array
+     *
+     * @param capacity : the expanded length of array
+     */
+    private void resize(int capacity){
+        int[] b = new int[capacity];
+        System.arraycopy(a,nextFirst + 1,b,0,a.length - nextFirst - 1);
+        if(nextFirst != 0){
+            System.arraycopy(a,0,b,a.length - nextFirst - 1, nextLast);
+        }
+        nextLast = a.length;
+        a = b;
+        nextFirst = a.length - 1;
+
+    }
+
+    public void addFirst(int x){
+        a[nextFirst] = x;
+        size += 1;
+        if(nextFirst == 0){
+            nextFirst = a.length;
+        }
+        nextFirst -= 1;
+        if(size == a.length){
+            resize(size * factor);
+        }
+
+
+    }
+    /** Inserts X into the back of the list. */
+    public void addLast(int x) {
+        a[nextLast] = x;
+        size += 1;
+        if(nextLast == a.length - 1){
+            nextLast = -1;
+        }
+        nextLast += 1;
+        if(size == a.length){
+            resize(size * factor);
+        }
+    }
+    public boolean isEmpty(){
+        return (size == 0);
+    }
+
+    /** Returns the item from the back of the list. */
+    public int getLast() {
+
+        return a[nextLast - 1];
+    }
+
+
+    /** Returns the number of items in the list. */
+    public int size() {
+        return size;
+    }
+
+    /** Prints the items in the deque from first to last
+     * seperated by a space.
+     */
+    public void printDeque(){
+        System.out.print("The items in the deque are: [ ");
+        if(nextFirst < nextLast){
+            for(int i = nextFirst +1; i < nextLast; i++){
+                System.out.print(a[i]+" ");
+            }
+        }
+        else{
+            for(int i = nextFirst + 1; i < a.length; i++){
+                System.out.print(a[i]+" ");
+            }
+            for(int j = 0; j < nextLast; j++ ){
+                System.out.print(a[j]+" ");
+            }
+        }
+
+            System.out.print("]");
+    }
+
+    /** Removes and returns the item at the front of the deque.
+     * if no such item exists, return null
+     */
+    public int removeFirst(){
+        if(nextFirst == a.length - 1){
+            nextFirst = -1;
+        }
+        nextFirst += 1;
+        size -= 1;
+        return a[nextFirst];
+
+    }
+    /** Deletes item from back of the list and
+     * returns deleted item. */
+    public int removeLast() {
+        if(nextLast == 0){
+            nextLast = a.length;
+        }
+        nextLast -= 1;
+        size -= 1;
+        return a[nextLast];
+    }
+
+    /** Gets the item at the given index, where 0 is the front
+     * if no such item exists, return null
+     * @param i
+     * @return i th item
+     */
+    public int get(int i) {
+        if(i + nextFirst + 1 >= a.length){
+            return a[i + nextFirst + 1 - a.length];
+        }
+        else if(i >= size){
+            return 0;
+        }
+        else{
+            return a[i + nextFirst + 1];
+        }
+    }
+    public static void main(String[] args){
+        ArrayDeque a = new ArrayDeque();
+        a.addFirst(3);
+        a.addFirst(2);
+        a.addFirst(1);
+        a.addLast(4);
+        a.addLast(5);
+        a.addLast(6);
+        a.addLast(7);
+        a.addLast(8);
+        a.addLast(9);
+        a.addLast(10);
+        a.printDeque();
+        System.out.println(a.getLast());
+        System.out.println(a.get(0));
+        System.out.println(a.get(8));
+        System.out.println(a.size());
+        a.removeLast();
+        a.removeLast();
+        a.removeFirst();
+        a.removeFirst();
+        a.removeFirst();
+        a.removeFirst();
+        a.removeFirst();
+        a.removeLast();
+        a.printDeque();
+
+    }
+}
